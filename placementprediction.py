c3e7e8a98d92f0883852ab9f0f77aca1f677ff3f
kumar291098 <63694950+kumar291098@users.noreply.github.com>
2023-10-28 22:58:02 +0530

    web app is created success fully you can run it

--- placementprediction.py
+++ placementprediction.py
@@ -5,3 +5,26 @@ Created on Sat Oct 28 22:03:04 2023
 @author: Avnish kumar
 """
 
+import numpy as np
+#import pandas as pd
+from PIL import Image
+import streamlit as st
+import pickle
+
+#load model using pickle
+lg=pickle.load(open('placement.pkl','rb'))
+#web app
+img=Image.open('placementImg.jpg')
+st.image(img, width=650)
+st.title(" placement prediction")
+input_text=st.text_input("Enter your all placement feature")
+
+if input_text:
+    input_list=input_text.split(',')
+    np_df=np.asarray(input_list, dtype=float)
+    prediction=lg.predict(np_df.reshape(1, -1))
+    if prediction[0]==1:
+        st.write("this student is placed")
+    else:
+        st.write("this student is not placed")
+        
